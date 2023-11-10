45b9ce7aa494ea87136fa49c36b4d4d9de4cc3f2
Lluis161200 <73023169+Lluis161200@users.noreply.github.com>
2023-11-10 10:37:55 +0100

    Update Quantizer.py

--- TCIProject/codingTools/Quantizer.py
+++ TCIProject/codingTools/Quantizer.py
@@ -13,16 +13,16 @@ class Quantizer:
         
     #quantize    
     def quantize(self, image_data, image_data_quantized):
-        for z in range(image_data.shape[0]-1):
-            for y in range(image_data.shape[1]-1):
-                for x in range(image_data.shape[2]-1):
-                    image_data_quantized[z][y][x] = int(sign_of_value(image_data[z][y][x]) * (image_data[z][y][x] / self.quantization_step))                
+        for z in range(image_data.shape[0]):
+            for y in range(image_data.shape[1]):
+                for x in range(image_data.shape[2]):
+                    image_data_quantized[z][y][x] = int(sign_of_value(image_data[z][y][x]) * abs((image_data[z][y][x]) / self.quantization_step))                
         
     #dequantize
     def dequantize(self, image_data_quantized, image_data_reconstructed):
-        for z in range(image_data_quantized.shape[0]-1):
-            for y in range(image_data_quantized.shape[1]-1):
-                for x in range(image_data_quantized.shape[2]-1):
+        for z in range(image_data_quantized.shape[0]):
+            for y in range(image_data_quantized.shape[1]):
+                for x in range(image_data_quantized.shape[2]):
                     image_data_reconstructed[z][y][x] = image_data_quantized[z][y][x] * self.quantization_step
                     
     
