1b3f142665400bcbe2da1205b8fd4da9373ed5e9
UserBraian <113941279+UserBraian@users.noreply.github.com>
2023-11-15 23:03:13 +0100

    Update Wavelet.py

--- TCIProject/codingTools/Wavelet.py
+++ TCIProject/codingTools/Wavelet.py
@@ -18,36 +18,56 @@ class Wavelet:
         
     def forward(self):
     
-        filas=self.original_image.shape[1]
-        columnas=self.original_image.shape[2]//2
+        filas = self.original_image.shape[1]
+        columnas = self.original_image.shape[2]//2
         print("f:", filas)
         print("c", columnas)
-        L=np.empty((filas,columnas))
-        H=np.empty((filas,columnas))
+        
+        #Hay que comprobar que la num de columnas sea un numero par, sino habra que replicar la ultima columna y añadirla a la matriz (imagen)
+        if((columnas%2)!=0):
+            #get ultima columna de la imagen
+            #añadir columna a matriz -> sth like reshape matriz
+            
+            
+
+        L = np.empty((filas,columnas))
+        H = np.empty((filas,columnas))
         
         a = 0
         b = 0
         
-        for z in range(self.original_image.shape[0]):
-            for i in range(self.original_image.shape[1]):
-                for j in range((self.original_image.shape[2]//2)): 
-                    a=int(self.original_image[z][i][2*j])
-                    b=int(self.original_image[z][i][2*j+1])
-                    op=b+int(int(a-b)/2)
+        
+        
+        #Forward horizontal - nos genera  la matriz L|H
+        for z in range(self.original_image.shape[0]): #Dimension
+            for i in range(self.original_image.shape[1]): #Filas (fijamos filas)
+                for j in range((self.original_image.shape[2]//2)): #Columnas (movemos por cada columna)
+                    a = int(self.original_image[z][i][2*j])
+                    b = int(self.original_image[z][i][2*j+1])
+                    op = b+int(int(a-b)/2)
                
-                    L[i][j]=op
-                    H[i][j]=(a-b)
+                    L[i][j] = op
+                    H[i][j] = (a-b)
+                            
+        #Forward vertical - Trabaja solo con la banda L y nos genera la matriz LL|H
+        #                                                                      LH|H
+                        #filas      ,    cols
+        Lv = np.empty((L.shape[0]//2,L.shape[1]))
+        Hv = np.empty((L.shape[0]//2,L.shape[1]))
+
+        for j in range(L.shape[1]): #Columnas (fijamos columna)
+            for i in range(L.shape[0]//2): #Filas (movemos por cada fila)
+                a_L = int(L[2*i][j])
+                b_L = int(L[2*i+1][j])
+                a_H = int(H[2*i][j])
+                b_H = int(H[2*i+1][j])
+                Lv[i][j] = int(b_L + int(int(a_L-b_L)/2))
+                Hv[i][j] = (a_H-b_H)
                     
-        for z in range(self.original_image.shape[0]):
-            for j in range(self.original_image.shape[2]): 
-                for i in range((self.original_image.shape[1]//2)-1):
-                    a=self.original_image[z][2*i][j]
-                    b=self.original_image[z][2*i+1][j]
-                    #Lv.append(b+((a-b)//2))
-                    #Hv.append(b-a)
-                    
-        print(L)
-        print(H)
+        print('Matriz L horizontal: \n',L) #matriz L - OK (values checked)
+        print('Matriz H horizontal: \n',H) #matriz H - OK (values checked)
+        print('Matriz Lv vertical: \n',Lv) #matriz LL - OK (values checked)
+        print('Matriz Hv vertical: \n',Hv) #matriz LH - OK (values checked)
         
     
     
