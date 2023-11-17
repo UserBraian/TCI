4afbe49e50577e28690558f2545866e47411589d
Lluis161200 <73023169+Lluis161200@users.noreply.github.com>
2023-11-17 12:12:04 +0100

    Update Wavelet.py

--- TCIProject/codingTools/Wavelet.py
+++ TCIProject/codingTools/Wavelet.py
@@ -16,7 +16,7 @@ class Wavelet:
         
         
         
-    def forward(self):
+    def forward(self, image_data_quantized):
     
         filas = self.original_image.shape[1]
         columnas = self.original_image.shape[2]//2
@@ -27,50 +27,58 @@ class Wavelet:
         if((columnas%2)!=0):
             #get ultima columna de la imagen
             #añadir columna a matriz -> sth like reshape matriz
-            
+            k=1
             
 
-        L = np.empty((filas,columnas))
-        H = np.empty((filas,columnas))
+       
         
         a = 0
         b = 0
+        c=0
+        niveles=0
+        result=image_data_quantized
+        L=image_data_quantized
+        H=image_data_quantized
+        matriz_actual_levels=self.original_image
+       
+        for x in range(self.levels):           
+            for z in range(matriz_actual_levels.shape[0]): #Dimension
+                for i in range(matriz_actual_levels.shape[1]): #Filas (fijamos filas)
+                    for j in range((matriz_actual_levels.shape[2]//2)): #Columnas (movemos por cada columna)
+                        
+                        a = int(matriz_actual_levels[z][i][2*j])
+                        b = int(matriz_actual_levels[z][i][2*j+1])
+                        op = b+int(int(a-b)/2)
+                        print(op)
+                        L[z][i][j] = op
+                        H[z][i][j] = int(a-b)
+                        result[z][i][j]=L[z][i][j]
+                        result[z][i][j+matriz_actual_levels.shape[2]//2]= H[z][i][j]
+            print(result)
+
+        #Forward vertical - Trabaja solo con la banda L y nos genera la matriz LL|
+        #                                                                      LH|
+                        #filas      ,    cols
+            LL = np.empty((matriz_actual_levels.shape[0],L.shape[1]//2,L.shape[2]))
+            LH = np.empty((matriz_actual_levels.shape[0], L.shape[1]//2,L.shape[2]))
+            for z in range(L.shape[0]): #Dimension
+                for j in range(L.shape[2]): #Columnas (fijamos columna)
+                    for i in range(L.shape[1]//2): #Filas (movemos por cada fila)
+                        a_L = int(L[z][2*i][j])
+                        b_L = int(L[z][2*i+1][j])
+                        LL[z][i][j] = int(b_L + int(int(a_L-b_L)/2))
+                        LH[z][i][j] = (a_L-b_L)
+            matriz_actual_levels=LL
+            niveles=niveles+1
         
-        
+
+            
         
         #Forward horizontal - nos genera  la matriz L|H
-        for z in range(self.original_image.shape[0]): #Dimension
-            for i in range(self.original_image.shape[1]): #Filas (fijamos filas)
-                for j in range((self.original_image.shape[2]//2)): #Columnas (movemos por cada columna)
-                    a = int(self.original_image[z][i][2*j])
-                    b = int(self.original_image[z][i][2*j+1])
-                    op = b+int(int(a-b)/2)
-               
-                    L[i][j] = op
-                    H[i][j] = (a-b)
-                            
-        #Forward vertical - Trabaja solo con la banda L y nos genera la matriz LL|H
-        #                                                                      LH|H
-                        #filas      ,    cols
-        Lv = np.empty((L.shape[0]//2,L.shape[1]))
-        Hv = np.empty((L.shape[0]//2,L.shape[1]))
-
-        for j in range(L.shape[1]): #Columnas (fijamos columna)
-            for i in range(L.shape[0]//2): #Filas (movemos por cada fila)
-                a_L = int(L[2*i][j])
-                b_L = int(L[2*i+1][j])
-                a_H = int(H[2*i][j])
-                b_H = int(H[2*i+1][j])
-                Lv[i][j] = int(b_L + int(int(a_L-b_L)/2))
-                Hv[i][j] = (a_H-b_H)
-                    
-        print('Matriz L horizontal: \n',L) #matriz L - OK (values checked)
-        print('Matriz H horizontal: \n',H) #matriz H - OK (values checked)
-        print('Matriz Lv vertical: \n',Lv) #matriz LL - OK (values checked)
-        print('Matriz Hv vertical: \n',Hv) #matriz LH - OK (values checked)
+        
+        
         
     
     
     
     
-
