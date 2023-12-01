fcb38f2b6d808b4abb15af864338c196b237710f
Lluis161200 <73023169+Lluis161200@users.noreply.github.com>
2023-12-01 11:51:48 +0100

    Update Prediction.py

--- TCIProject/codingTools/Prediction.py
+++ TCIProject/codingTools/Prediction.py
@@ -94,5 +94,53 @@ class Prediction:
         
     
     
+    def GodPrediction(self, empty_image):
+        image_data_empty = ImageRaw.get_image_structure_empty()
+        pred1 = Prediction(original_image)
+        imagen_prima1 = pred1.north(image_data_empty)
+        residu1 = original_image-imagen_prima1
     
-
+        q=Quantizer(1,10)
+        img_q=ImageRaw.get_image_structure_empty()
+        q.quantize(residu1, img_q)
+        
+        img_desq=ImageRaw.get_image_structure_empty()
+        q.dequantize(img_q, img_desq)
+        dimension = img_desq.shape[0]
+        filas = img_desq.shape[1]
+        columnas = img_desq.shape[2]
+        result = np.copy(empty_image)
+        p=ImageRaw.get_image_structure_empty()
+        x_barret=ImageRaw.get_image_structure_empty()
+        for dim in range(dimension):
+            for i in range(filas):
+                for j in range(columnas):     
+                    if j=0:
+                        p[dim][i][j]=0
+                    else:                      
+                        p[dim][i][j]=p[dim][i][j-1]+img_desq[dim][i][j]
+        
+        
+        x_barret=img_desq+p
+        
+        return x_barret
+    
+    def LiniaPrincipal(self, x_barret):
+        
+        image_data_empty = ImageRaw.get_image_structure_empty()
+        pred1 = Prediction(x_barret)
+        imagen_prima1 = pred1.north(image_data_empty)
+        residu1 = original_image-imagen_prima1
+    
+        q=Quantizer(1,10)
+        img_q=ImageRaw.get_image_structure_empty()
+        q.quantize(residu1, img_q)
+        
+        img_desq=ImageRaw.get_image_structure_empty()
+        q.dequantize(img_q, img_desq)
+        
+        aux_img=img_desq+imagen_prima1
+        m=Metrics(self.original_image, aux_img)
+        m.compute()
+        
+        return aux_img
